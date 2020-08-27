bb125ba5ab68754a9b44d57b7fd77a1d241b0ad9
brainstew927 <brainstew927@gmail.com>
2020-08-28 01:36:39 +0200

    first day changes partially working grid and working graphics

---
+++ include/BaseMathFunctions.h
@@ -0,0 +1,18 @@
+#pragma once
+#include<math.h>
+
+// function to calculate the area of the whole window
+int windowArea(int height, int width){
+    return height*width;
+}
+// functions to calculate the area of a single square
+int squareArea(int windowArea, int numberOfSquare){
+    return windowArea / numberOfSquare;
+}
+int squareArea(int height, int width, int numberOfSquare){
+    return windowArea(height, width) / numberOfSquare;
+}
+// function that returns the size of the size of the square
+float squareSide(int squareArea){
+    return sqrt(squareArea);
+}
---
+++ src/Main.cpp
@@ -0,0 +1,58 @@
+#include <SFML/Graphics.hpp>
+#include "../include/BaseMathFunctions.h"
+#include<iostream>
+
+using namespace std;
+
+
+#define WINDOW_HEIGHT 800
+#define WINDOW_WIDTH  600
+
+#define CELL_NUMBER 7500
+
+int main()
+{
+    // create the window
+    sf::RenderWindow window(sf::VideoMode(WINDOW_HEIGHT, WINDOW_WIDTH), "Game of Life");
+    
+    float cellSide = squareSide(squareArea(windowArea(WINDOW_HEIGHT, WINDOW_WIDTH), CELL_NUMBER));
+    cout << "lato quad: " << cellSide << endl;
+    cout << "WINDOW_WIDTH/cellSide: " << WINDOW_WIDTH/cellSide << endl;
+    cout << " WINDOW_HEIGHT/ cellSide: " <<  WINDOW_HEIGHT/ cellSide << endl;
+   
+    
+    // run the program as long as the window is open
+    while (window.isOpen())
+    {
+        // check all the window's events that were triggered since the last iteration of the loop
+        sf::Event event;
+        while (window.pollEvent(event))
+        {
+            // "close requested" event: we close the window
+            if (event.type == sf::Event::Closed)
+                window.close();
+        }
+
+        // clear the window with black color
+        window.clear(sf::Color::Black);
+
+        // draw everything here...
+        // window.draw(...);
+        
+        
+        for(int y = 0; y < WINDOW_HEIGHT/ cellSide; y++){
+            for(int x = 0; x < WINDOW_WIDTH/cellSide; x++){
+                sf::RectangleShape rs(sf::Vector2f(cellSide, cellSide));
+                rs.setOutlineThickness(1.f);
+                rs.setOutlineColor(sf::Color(0, 0, 0));
+
+                rs.setPosition(x*cellSide, y*cellSide);
+                window.draw(rs);
+            }
+        }
+        // end the current frame
+        window.display();
+    }
+
+    return 0;
+}
