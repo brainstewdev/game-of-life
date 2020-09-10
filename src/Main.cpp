c349929cad422a0ebf5173dd21e4182d6036ec04
brainstew927 <brainstew927@gmail.com>
2020-09-11 00:37:50 +0200

    created the renderer class now the graphical part is handled by the renderer class instead of being handled into the Main.cpp file ATTENTION: it produces a seg fault for now.

--- src/Main.cpp
+++ src/Main.cpp
@@ -4,6 +4,7 @@
 #include "../include/gameClasses.hpp"
 #include "../include/gameLogic.hpp"
 #include "../include/eventsHandler.hpp"
+#include "../include/Renderer.hpp"
 
 // the desired height and width of the window
 constexpr int WINDOW_HEIGHT = 600;
@@ -23,7 +24,8 @@ int main(){
     // creates the plane that is going to handle the true game of life, 
     // the rest of it is just to display what is going on
     Plane gamePlane(WINDOW_HEIGHT, WINDOW_WIDTH, CELL_NUMBER);
-
+    // the renderer object basically manages all the graphics
+    Renderer renderer(gamePlane, window);
     // the seed used to generate the plane
     int seed = 2024234;
 
@@ -46,45 +48,8 @@ int main(){
                 handleEvent(event, gamePlane, onPause);
             }
         }
-
-        // clear the window with black color
-        window.clear(sf::Color::White);
+        renderer.render();
         
-        sf::VertexArray vertArray(sf::Lines, 2);
-        // draw the grid
-        // draw the vertical lines
-        for(int i = 0; i < gamePlane.getWidthInCell(); i++){
-            // draw the line going from y = 0 x = gamePlane.getCellside() to the bottom
-            vertArray[0] = sf::Vertex(sf::Vector2f(i*gamePlane.getCellside(), 0.0f), sf::Color::Black);
-            vertArray[1] = sf::Vertex(sf::Vector2f(i*gamePlane.getCellside(), gamePlane.getCellside()*gamePlane.getHeightInCell()), sf::Color::Black);
-            window.draw(vertArray);
-        }
-        // draw the horizontal lines
-        for(int i = 0; i < gamePlane.getHeightInCell(); i++){
-            // draw the line going from y = 0 x = gamePlane.getCellside() to the bottom
-            vertArray[0] = sf::Vertex(sf::Vector2f(0.0f, i*gamePlane.getCellside()), sf::Color::Black);
-            vertArray[1] = sf::Vertex(sf::Vector2f(gamePlane.getCellside()*gamePlane.getWidthInCell(), i*gamePlane.getCellside()), sf::Color::Black);
-            window.draw(vertArray);
-        }
-
-        // pre create a live cell
-        sf::RectangleShape AliveCell(sf::Vector2f(cellSide, cellSide));
-        AliveCell.setOutlineColor(sf::Color(255, 0, 0)); 
-        AliveCell.setFillColor(sf::Color::Black);
-        AliveCell.setOutlineThickness(1.f);
-        for(int y = 0; y < gamePlane.getHeightInCell(); y++){
-            for(int x = 0; x < gamePlane.getWidthInCell(); x++){
-                sf::RectangleShape * currentCell;
-                if(gamePlane.cellIsAlive(x,y)){
-                    currentCell = &AliveCell;
-                    currentCell->setPosition(x*cellSide, y*cellSide);
-                     // finally, draw the shape!
-                    window.draw(*currentCell);
-                }
-            }
-        }
-        // end the current frame, display what has been drawn
-        window.display();
         // update the plane using the various logic functions (they can be found in ../include/gameLogic.h)
         if(!(onPause))
             updatePlane(gamePlane);
